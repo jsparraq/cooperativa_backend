4254430b0af52a61ccb9bdae6eeaaffee4a7f320
Sebastian Parra <sebastian.parra96@gmail.com>
2019-10-05 20:09:41 -0500

    Update config file

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,6 @@ const dotenv = require('dotenv');
 dotenv.config({ silent: process.env.NODE_ENV === 'production' });
 
 module.exports = {
-  port: process.env.PORT || 4000,
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/cooperativa',
+  port: process.env.PORT,
+  mongoURL: process.env.MONGO_URL,
 };
