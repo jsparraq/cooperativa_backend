862750b145693215245ecb692e284f8a07d44a2e
jsparraq <sebastian.parra96@gmail.com>
2019-10-22 21:10:36 -0500

    Upgrade handler errors

--- src/errors/handlerErrors.js
+++ src/errors/handlerErrors.js
@@ -14,9 +14,13 @@ exports.handlerError = async err => {
   } else {
     return { status: 500, body: { type: 'Error', message: 'Something went wrong. Please try again.', name: 'Error' } };
   }
-  const message = customError.getMessage();
-  const name = customError.getName();
-  const status = customError.getStatus();
-  const type = customError.getType();
-  return { status, body: { type, message, name } };
+  try {
+    const message = customError.getMessage();
+    const name = customError.getName();
+    const status = customError.getStatus();
+    const type = customError.getType();
+    return { status, body: { type, message, name } };
+  } catch (err) {
+    return { ...err, ...{ status: 500 } };
+  }
 };
